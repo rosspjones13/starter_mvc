1e1cbfb19ee83cd8b980029ccb5f3d800c936861
Ross Jones <rossmosis.jones@gmail.com>
2019-01-30 16:29:04 -0500

    added create actions for actors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,37 @@
 class ApplicationController < Sinatra::Base
   set :views, "app/views"
 
+  #puts user at the actors index
   get '/' do
     redirect to "/actors"
   end
 
+  #gets all actors into index
   get '/actors' do
     @actors = Actor.all
     erb :index
   end
 
-  # get '/actors/new' do
-    
-  #   erb :new
-  # end
+  #form to create new actor
+  get '/actors/new' do
+    erb :new
+  end
+
+  #create actor with a movie
+  post '/actors' do
+    actor = Actor.find_or_create_by(name: params[:name])
+    params[:film].each do |f|
+      if f[:title] != ""
+        Film.create(title: params[:title]
+                    year: params[:year]
+                    genre: params[:genre])
+      end
+    end
+    redirect "/actors/#{actor.id}"
+  end
+
+  get '/actors/:id' do
+    @actor = Actor.find(params[:id])
+    erb :show
+  end
 end
