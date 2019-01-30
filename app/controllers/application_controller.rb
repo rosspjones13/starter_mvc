e7bb8e4f75b10a2dcc034dfd2b319bcd8e40c694
Ross Jones <rossmosis.jones@gmail.com>
2019-01-30 17:35:09 -0500

    added all crud for actors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
 
   #puts user at the actors index
   get '/' do
-    redirect to "/actors"
+    redirect "/actors"
   end
 
   #gets all actors into index
@@ -22,16 +22,36 @@ class ApplicationController < Sinatra::Base
     actor = Actor.find_or_create_by(name: params[:name])
     params[:film].each do |f|
       if f[:title] != ""
-        Film.create(title: params[:title]
-                    year: params[:year]
+        Film.create(title: params[:title],
+                    year: params[:year],
                     genre: params[:genre])
       end
     end
     redirect "/actors/#{actor.id}"
   end
 
+  #shows a page of an actor and their films
   get '/actors/:id' do
     @actor = Actor.find(params[:id])
     erb :show
+  end
+
+  #form to edit an actor's info
+  get '/actors/:id/edit' do
+    @actor = Actor.find(params[:id])
+    erb :edit
+  end
+
+  #edit actors name
+  patch '/actors/:id' do
+    @actor = Actor.find(params[:id])
+    @actor.update(params[:name])
+    redirect "/actors/#{actor.id}"
+  end
+
+  #destroy an actor
+  delete '/actors/:id' do
+    Actor.destroy(params[:id])
+    redirect "/actors"
   end
 end
