1f9913461dc1a329305a637b54400dd90d2c55d1
Ross Jones <rossmosis.jones@gmail.com>
2019-01-30 15:49:00 -0500

    adding actors controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < Sinatra::Base
+  set :views, "app/views"
 
+  get '/' do
+    redirect to "/actors"
+  end
+
+  get '/actors' do
+    @actors = Actor.all
+    erb :index
+  end
+
+  # get '/actors/new' do
+    
+  #   erb :new
+  # end
 end
